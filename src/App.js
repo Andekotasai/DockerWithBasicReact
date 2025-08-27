d9130e756d4f282e913952175095ca48a1ea32b9
saikotabhadharao <andesaikotabhadharao@gmail.com>
2025-08-27 16:42:58 +0530

    docker Learning

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Learn react</h3>
+        <h2>Currently i am working as a developer</h2>
+        <h3>i have 3 years experience - Cognizant</h3>
+        <p>New Course</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
